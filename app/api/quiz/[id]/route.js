69dbe7c188608fad02d1bae7f154553006b49c4a
cheboi <moscheb96gmail.com>
2023-09-15 12:10:01 +0300

    created dynamic API route for questions

--- app/api/quiz/[id]/route.js
+++ app/api/quiz/[id]/route.js
@@ -11,6 +11,19 @@
 import questions from '@/data/quiz.json'
 import { NextResponse } from 'next/server'
 
-export async function GET() {
-  return NextResponse.json({})
+export async function GET(req, { params }) {
+  try {
+    const question = questions.data.find(item => item.id === params.id)
+    if (!question) {
+      // throw new Error('Not Found', { status: 400 })
+      return new NextResponse('not found', { status: 400 })
+    }
+
+    const { correct_answer, ...rest } = question
+    return NextResponse.json({
+      question: rest,
+    })
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
 }
